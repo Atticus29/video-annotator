738b3b8f902533c235995c9d30af9045518630d4
Mark <mark.aaron.fisher@gmail.com>
2023-01-21 16:16:50 -0800

    isolate the firebase config and separate it from the secret keys in order to accommodate environment variables more easily. Remove and comment out some other code in an intermediate stage of refactoring the firebase auth stuff in order to prevent test failures and build failures

---
+++ keys.ts
@@ -0,0 +1,9 @@
+export const secrets = {
+  apiKey: "AIzaSyAkyK0eEz1GLUoit_oGBPM97NVJUcrxz-o",
+  authDomain: "annotate-video-a8783.firebaseapp.com",
+  projectId: "annotate-video-a8783",
+  storageBucket: "annotate-video-a8783.appspot.com",
+  messagingSenderId: "516593953614",
+  appId: "1:516593953614:web:9c2fe7276c32cc47b1cc1f",
+  measurementId: "G-B9SRTYG55C",
+};
