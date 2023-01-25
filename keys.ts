59d1925b28c78c16217314b1e2a3dc00393827f8
Mark <mark.aaron.fisher@gmail.com>
2023-01-25 11:54:45 -0800

    implement an AuthContext

--- keys.ts
+++ keys.ts
@@ -7,3 +7,24 @@ export const secrets = {
   appId: "1:516593953614:web:9c2fe7276c32cc47b1cc1f",
   measurementId: "G-B9SRTYG55C",
 };
+// export const NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY =
+//   "AIzaSyAkyK0eEz1GLUoit_oGBPM97NVJUcrxz-o";
+// export const NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN =
+//   "annotate-video-a8783.firebaseapp.com";
+// export const NEXT_PUBLIC_FIREBASE_PROJECT_ID = "annotate-video-a8783";
+// export const NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET =
+//   "annotate-video-a8783.appspot.com";
+// export const NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID = "516593953614";
+// export const NEXT_PUBLIC_FIREBASE_APP_ID =
+//   "1:516593953614:web:9c2fe7276c32cc47b1cc1f";
+// export const NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID = "G-B9SRTYG55C";
+
+// export const NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY = "AIzaSyAkyK0eEz1GLUoit_oGBPM97NVJUcrxz-o";
+
+// authDomain: "annotate-video-a8783.firebaseapp.com",
+// projectId: "annotate-video-a8783",
+// storageBucket: "annotate-video-a8783.appspot.com",
+// messagingSenderId: "516593953614",
+// appId: "1:516593953614:web:9c2fe7276c32cc47b1cc1f",
+//   measurementId: "G-B9SRTYG55C",
+// };
