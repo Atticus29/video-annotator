0e676de28529510ddeb19bfa00acc57c8b37e621
Mark <mark.aaron.fisher@gmail.com>
2023-12-14 12:16:23 -0800

    Individual creation (#77)
    * add TODO
    
    * implement individual creation form

--- components/IndividualIntake/index.tsx
+++ components/IndividualIntake/index.tsx
@@ -11,51 +11,56 @@ import SingleFormField from "../SingleFormField";
 import useFirebaseAuth from "../../hooks/useFirebaseAuth";
 import { IntlShape, useIntl } from "react-intl";
 
-const VideoIntake: React.FC<{
+const IndividualIntake: React.FC<{
   collection: Collection;
   onCloseDialog?: () => void;
 }> = ({ collection, onCloseDialog }) => {
   const intl: IntlShape = useIntl();
+  const defaultIndividualName: string = intl.formatMessage({
+    id: "INDIVIDUAL",
+  });
   const { user, authError } = useFirebaseAuth();
   const [localCollection, setLocalCollection] = useState<Collection>();
-  const [videoQuestionFormValues, setVideoQuestionFormValues] = useState<{}>(
-    {}
-  );
+  const [individualQuestionFormValues, setIndividualQuestionFormValues] =
+    useState<{}>({});
   const [
-    arevideoQuestionFormValuesInvalid,
-    setArevideoQuestionFormValuesInvalid,
+    areIndividualQuestionFormValuesInvalid,
+    setAreIndividualQuestionFormValuesInvalid,
   ] = useState<{}>({});
 
-  const videoQuestionsFormFieldGroup: FormFieldGroup = useMemo(() => {
+  const individualQuestionsFormFieldGroup: FormFieldGroup = useMemo(() => {
     return {
       title: "VideoFormFieldGroupForTheLocalCollection",
-      setValues: setVideoQuestionFormValues,
-      actualValues: videoQuestionFormValues,
-      isInvalids: arevideoQuestionFormValuesInvalid,
-      setIsInvalids: setArevideoQuestionFormValuesInvalid,
+      setValues: setIndividualQuestionFormValues,
+      actualValues: individualQuestionFormValues,
+      isInvalids: areIndividualQuestionFormValuesInvalid,
+      setIsInvalids: setAreIndividualQuestionFormValuesInvalid,
     };
-  }, [arevideoQuestionFormValuesInvalid, videoQuestionFormValues]);
+  }, [areIndividualQuestionFormValuesInvalid, individualQuestionFormValues]);
 
   useEffect(() => {
     const initialCollection = { ...collection };
-    initialCollection.videoQuestionsFormFieldGroup =
-      videoQuestionsFormFieldGroup;
+    initialCollection.individualQuestionsFormFieldGroup =
+      individualQuestionsFormFieldGroup;
     setLocalCollection(initialCollection);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user]);
 
   const titleId: string = intl.formatMessage(
-    { id: "SUBMIT_NEW_VIDEO", defaultMessage: "Submit new {videoName}" },
-    { videoName: collection.nameOfVideo }
+    { id: "SUBMIT_NEW_INDIVIDUAL", defaultMessage: "Submit new {videoName}" },
+    { individualName: collection?.nameOfIndividual || defaultIndividualName }
   );
 
   const bodyId: string = intl.formatMessage(
     {
-      id: "VIDEO_INTAKE_DETAILS",
+      id: "INDIVIDUAL_INTAKE_DETAILS",
       defaultMessage:
         "To add a {videoName} to the collection, fill out the form below.",
     },
-    { videoName: collection?.nameOfVideo?.toLowerCase() }
+    {
+      individualName: collection?.nameOfIndividual?.toLowerCase(),
+      videoName: collection?.nameOfVideo?.toLowerCase(),
+    }
   );
 
   return (
@@ -68,14 +73,14 @@ const VideoIntake: React.FC<{
       <InfoPanelBody bodyId={bodyId} bodyDefault={bodyId} />
       <Grid container>
         {map(
-          localCollection?.videoIntakeQuestions,
+          localCollection?.individualIntakeQuestions,
           (intakeQuestion, intakeQuestionIdx) => {
             if (intakeQuestion) {
               return (
                 <Grid item lg={12} sm={12} key={intakeQuestionIdx}>
                   <SingleFormField
                     question={intakeQuestion}
-                    formFieldGroup={videoQuestionsFormFieldGroup}
+                    formFieldGroup={individualQuestionsFormFieldGroup}
                     key={intakeQuestionIdx}
                   />
                 </Grid>
@@ -84,14 +89,16 @@ const VideoIntake: React.FC<{
           }
         )}
         {/* @TODO add invidual addition */}
-        {localCollection?.videoQuestionsFormFieldGroup &&
-          localCollection?.videoIntakeQuestions && (
+        {localCollection?.individualQuestionsFormFieldGroup &&
+          localCollection?.individualIntakeQuestions && (
             <Grid item lg={12} sm={12}>
               <ComposedFormSubmissionButton
-                questionsOfConcern={localCollection?.videoIntakeQuestions || []}
-                formFieldGroupOfConcern={videoQuestionsFormFieldGroup}
+                questionsOfConcern={
+                  localCollection?.individualIntakeQuestions || []
+                }
+                formFieldGroupOfConcern={individualQuestionsFormFieldGroup}
                 collectionPath={localCollection?.urlPath}
-                collectionPropToUpdate={"videos"}
+                collectionPropToUpdate={"individuals"}
                 onCloseDialog={onCloseDialog}
               />
             </Grid>
@@ -100,4 +107,4 @@ const VideoIntake: React.FC<{
     </InfoPanel>
   );
 };
-export default VideoIntake;
+export default IndividualIntake;
